60a6829a067def8743ef7d0b3a99fa5764e4d800
Marwa Bakr <marwab2479@hstat.org>
2021-03-18 00:27:22 +0000

    update repo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/25th-nujf.json',
     dataType: 'json',
-    success: function(data) {
-        console.log(data);
-    }
-});
+    success: function(data) { 
+        alert("Hey are you lost on what to name you child? We can do that for you.");
+        
+        var userName = prompt("Find a popular baby name!, input any first letter and we'll give you a list of popular names the begin with that letter!");
+        userName = "";
+        }});//the input value or first 
+        
+
+for(var i = 0; i < data.length; i++) {
+    if(data[i].nm.substr(userName) === 0) { 
+document.write("Here are all names beginning with" + userName + (data[i].nm))}
+        } //prints out names that based off the user input 
